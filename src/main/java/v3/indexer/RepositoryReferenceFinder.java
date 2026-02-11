bda0ab9453a24a375adc08b5dd923fa93ad20a3f
Kenan Erarslan <kenan.erarslan@ing.com>
2026-02-11 11:07:58 +0300

    compile problem fixed

--- src/main/java/v3/indexer/RepositoryReferenceFinder.java
+++ src/main/java/v3/indexer/RepositoryReferenceFinder.java
@@ -1,7 +1,16 @@
 package v3.indexer;
 
+import v3.model.ServiceMethod;
+import v3.model.TableRepositoryMapping;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * Builds an index mapping mapper methods to service methods.
  */
 public class RepositoryReferenceFinder {
+    public Map<String, List<ServiceMethod>> findReferences(List<TableRepositoryMapping> repoMappings) {
+        return null;
+    }
 }
